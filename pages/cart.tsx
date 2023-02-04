79485d8b91f3178518922169ae2eaf1523d2d931
JamesWoo <JamesWoo>
2023-02-04 15:40:07 +0900

    찜 리스트

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,7 +1,12 @@
 import { CountControl } from '@components/CountControl'
 import styled from '@emotion/styled'
+import { Button } from '@mantine/core'
+import { products } from '@prisma/client'
 import { IconRefresh, IconX } from '@tabler/icons'
+import { useQuery } from '@tanstack/react-query'
+import { CATEGORY_MAP } from 'constants/products'
 import Image from 'next/image'
+import { useRouter } from 'next/router'
 import { useEffect, useMemo, useState } from 'react'
 
 interface CartItem {
@@ -14,6 +19,7 @@ interface CartItem {
 }
 
 export default function Cart() {
+  const router = useRouter()
   const [data, setData] = useState<CartItem[]>([])
 
   const deliveryAmount = 5000
@@ -48,14 +54,33 @@ export default function Cart() {
     setData(mockData)
   }, [])
 
+  const { data: products } = useQuery<
+    { items: products[] },
+    unknown,
+    products[] // prisma 에서 가져옴
+  >(
+    [`/api/get-products?skip=0&take=3`],
+    () => fetch(`/api/get-products?skip=0&take=3`).then((res) => res.json()),
+    {
+      select: (data) => data.items,
+    }
+  )
+
+  const handleOrder = () => {
+    //todo  주문하기 기능구현
+    alert(`장바구니에 담긴 것들 ${JSON.stringify(data)} 구현`)
+  }
+
   return (
-    <div>
+    <div style={{ width: '600px' }}>
       <span>cart {data.length}</span>
-      <div className="flex">
+      <div className="flex ">
         <div className="flex flex-col p-4 space-y-4 flex-1">
-          {data.map((item, idx) => (
-            <Item key={idx} {...item} />
-          ))}
+          {data?.length > 0 ? (
+            data.map((item, idx) => <Item key={idx} {...item} />)
+          ) : (
+            <div>장바구니에 아무것도 없습니다.</div>
+          )}
         </div>
         <div className="px-4">
           <div
@@ -80,18 +105,61 @@ export default function Cart() {
               <span className="font-semibold text-red-700">
                 {(amount + deliveryAmount + discountAmount).toLocaleString(
                   'ko-kr'
-                )}{' '}
+                )}
                 원
               </span>
             </Row>
+            <Button
+              style={{ backgroundColor: 'black' }}
+              radius="xl"
+              size="md"
+              styles={{ root: { height: 48 } }}
+              onClick={handleOrder}
+            >
+              구매하기
+            </Button>
           </div>
         </div>
+      </div>
+      <div className="mt-10">
+        <p>추천상품</p>
+        {products && (
+          <div className="gird grid-cols-3 gap-3">
+            {products.map((item) => (
+              <div
+                key={item.id}
+                style={{ maxWidth: 300 }}
+                onClick={() => router.push(`/products/${item.id}`)}
+              >
+                <Image
+                  className="rounded-2xl"
+                  src={item.image_url ?? ''}
+                  width={300}
+                  height={200}
+                  alt={item.name}
+                  placeholder="blur"
+                  blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mN8HyBYDwAFVwHR7ElIRgAAAABJRU5ErkJggg=="
+                />
+                <div className="flex">
+                  <span>{item.name}</span>
+                  <span className="m-auto">
+                    {item.price.toLocaleString('ko-KR')} 원
+                  </span>
+                </div>
+                <span className="text-zinc-400">
+                  {CATEGORY_MAP[item.category_id - 1]}
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
 }
 
 const Item = (props: CartItem) => {
+  const router = useRouter()
   const [quantity, setQuantity] = useState<number | undefined>(props.quantity)
 
   const [amount, setAmount] = useState<number>(props.quantity)
@@ -101,6 +169,16 @@ const Item = (props: CartItem) => {
     }
   }, [quantity, props.price])
 
+  const handleUpdate = () => {
+    // TODO: 장바구니 수정기능 구현
+    alert(`장바구니에서 ${props.name} 수정`)
+  }
+
+  const handleDelete = () => {
+    // TODO: 장바구니 삭제기능 구현
+    alert(`장바구니에서 ${props.name} 삭제`)
+  }
+
   // const amount = useMemo(() => {
   //   if (quantity != null) {
   //     return quantity * props.price
@@ -108,8 +186,14 @@ const Item = (props: CartItem) => {
   // }, [quantity, props.price])
 
   return (
-    <div className="w-full flex p-4">
-      <Image src={props.image_url} width={155} height={195} alt={props.name} />
+    <div className="w-full flex p-4" style={{ borderBottom: '1px solid grey' }}>
+      <Image
+        src={props.image_url}
+        width={155}
+        height={195}
+        alt={props.name}
+        onClick={() => router.push(`/products/${props.productId}`)}
+      />
       <div className="flex flex-col ml-4">
         <span className="font-semibold mb-2">{props.name}</span>
         <span className="mb-auto">
@@ -117,12 +201,12 @@ const Item = (props: CartItem) => {
         </span>
         <div className="flex items-center space-x-4">
           <CountControl value={quantity} setValue={setQuantity} max={20} />
-          <IconRefresh />
+          <IconRefresh onClick={handleUpdate} />
         </div>
       </div>
       <div>
         <span>{amount.toLocaleString('ko-kr')}</span>
-        <IconX />
+        <IconX onClick={handleDelete} />
       </div>
     </div>
   )
